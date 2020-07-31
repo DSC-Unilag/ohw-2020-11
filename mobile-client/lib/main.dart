b65b00eabb970124fb8763ff1c37d4bec515cb39
Mastersam <abadasamuelosp@gmail.com>
2020-07-31 00:58:12 -0700

    bind framework to engine

--- mobile-client/lib/main.dart
+++ mobile-client/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:budget_app/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(BudgetApp());
 }
 
